dc5558b0e9744421734be37d158881ddf4964f2c
Andrew Bruce <me@andrewbruce.net>
2022-03-28 09:15:44 +0100

    mix new loggerinner

---
+++ lib/loggerinner.ex
@@ -0,0 +1,18 @@
+defmodule Loggerinner do
+  @moduledoc """
+  Documentation for `Loggerinner`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Loggerinner.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/loggerinner_test.exs
@@ -0,0 +1,8 @@
+defmodule LoggerinnerTest do
+  use ExUnit.Case
+  doctest Loggerinner
+
+  test "greets the world" do
+    assert Loggerinner.hello() == :world
+  end
+end
