7633eaf2017c4af0a08066e1a64e5d6635356d87
Andrew Bruce <me@andrewbruce.net>
2022-03-29 15:00:38 +0100

    Automatically open downloaded .ica file

--- lib/loggerinner/cli.ex
+++ lib/loggerinner/cli.ex
@@ -9,7 +9,20 @@ defmodule Loggerinner.CLI do
     )
   end
 
+  def latest_ica() do
+    downloads = "#{System.get_env("HOME")}/Downloads"
+
+    [{path, _} | _] =
+      Path.wildcard("#{downloads}/*.ica")
+      |> Enum.map(&{&1, File.stat!(&1).ctime})
+      |> Enum.sort(fn {_, a}, {_, b} -> a >= b end)
+
+    path
+  end
+
   def log_in(username, password, passcode) do
+    initial_ica = latest_ica()
+
     Hound.start_session(browser: :chrome, user_agent: :chrome_desktop)
 
     navigate_to("https://wgdc.exconnect.hsbc.co.uk/logon/LogonPoint/tmindex.html")
@@ -25,6 +38,24 @@ defmodule Loggerinner.CLI do
     download_link = find_element(:class, "storeapp-details-link")
 
     click(download_link)
-    # Hound.end_session()
+
+    latest_file =
+      Enum.reduce_while(1..10, "", fn n, acc ->
+        case {n, initial_ica, latest_ica()} do
+          {n, same, same} ->
+            IO.puts("Waiting for download to complete #{n}")
+            :timer.sleep(1000)
+            {:cont, acc}
+
+          {_, _initial, latest} ->
+            IO.puts("Download complete!")
+            {:halt, latest}
+        end
+      end)
+
+    Hound.end_session()
+
+    IO.puts("opening #{latest_file}")
+    System.cmd("gio", ["open", latest_file])
   end
 end
